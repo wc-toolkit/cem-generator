0e5ee37f68169447d0816ba2b84c45d5faa1c182
Burton Smith <burton@breakstuff.io>
2026-09-11 07:48:40 -0400

    add GH pages deployment

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  base: "/cem-generator",
   integrations: [
     starlight({
       title: "CEM Generator",
